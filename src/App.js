3b088c4afae9e1b8676b5bc05353f99a6db16f6b
vnavarro <vnavarro@brightcove.com>
2021-11-06 17:43:04 -0600

    List of Categories with info

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import { Category } from './components/Category'
-
+import { ListOfCategories } from './components/ListOfCategories'
+import { GlobalStyle } from './GlobalStyles'
 export const App = () => (
-  <Category/>
+  <>
+    <ListOfCategories />
+    <GlobalStyle />
+  </>
 )
