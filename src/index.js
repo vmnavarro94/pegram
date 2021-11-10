94567c2593508772f19c789daaa96d4e46b97210
vnavarro <vnavarro@brightcove.com>
2021-11-10 08:59:53 -0600

    get photos with graphql

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const client = new ApolloClient({
+  uri: 'https://petgram-server-vnav-vnavarro.vercel.app/graphql'
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>, document.getElementById('app'))
