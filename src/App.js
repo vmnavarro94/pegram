092184131ff6d603c703f29a52a72ea3304db65c
vnavarro <vnavarro@brightcove.com>
2021-11-11 10:33:08 -0600

    Change detail routing to use link

--- src/App.js
+++ src/App.js
@@ -2,27 +2,19 @@ import React from 'react'
 import { Router } from '@reach/router'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
-import { PhotoCardWithQuery } from './containers/PhotoCardWithQuery'
 import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
-
   return (
     <>
       <GlobalStyle />
       <Logo />
-      {
-        detailId
-          ? <PhotoCardWithQuery id={detailId} />
-          : (
-            <Router>
-              <Home path='/' />
-              <Home path='/pet/:categoryId' />
-            </Router>
-            )
-      }
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:categoryId' />
+        <Detail path='/detail/:detailId' />
+      </Router>
     </>
   )
 }
