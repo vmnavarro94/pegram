7cb9900b9abbe45be10cc644706020b26d614f3d
vnavarro <vnavarro@brightcove.com>
2021-11-11 10:23:17 -0600

    Use Link to redirect

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ export const App = () => {
           : (
             <Router>
               <Home path='/' />
-              <Home path='/pet/:id' />
+              <Home path='/pet/:categoryId' />
             </Router>
             )
       }
