86c50625303e8be8404973136dbdc8033d885f96
vnavarro <vnavarro@brightcove.com>
2021-11-17 13:52:59 -0600

    Handle like with JWS

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { ImgWrapper, Img, Article } from './styles'
-import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { FavButton } from '../FavButton'
 import { ToggleLikeMutation } from '../../containers/ToggleLikeMutation'
@@ -8,9 +7,7 @@ import { Link } from '@reach/router'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const key = `like-${id}`
-  const [liked, setLiked] = useLocalStorage(key, false)
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
 
   return (
@@ -27,12 +24,11 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               {
                 (toggleLike) => {
                   const hangleFavClick = () => {
-                    !liked && toggleLike({
+                    toggleLike({
                       variables: {
                         input: { id }
                       }
                     })
-                    setLiked(!liked)
                   }
 
                   return <FavButton liked={liked} likes={likes} onClick={hangleFavClick} />
