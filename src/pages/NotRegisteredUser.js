5cfc92973ba9547e00d7e1191bb05ca10f5576fe
vnavarro <vnavarro@brightcove.com>
2021-11-17 12:39:20 -0600

    handle loading form

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -11,14 +11,16 @@ export const NotRegisteredUser = () => (
           <>
             <RegisterMutation>
               {
-                (register) => {
+                (register, { data, loading, error }) => {
                   const onSubmit = ({ email, password }) => {
                     const input = { email, password }
                     const variables = { input }
                     register({ variables }).then(activateAuth)
                   }
 
-                  return <UserForm onSubmit={onSubmit} title='Registrarse' />
+                  const errorMsg = error && 'El usuario ya existe o hay algún proglema.'
+
+                  return <UserForm onSubmit={onSubmit} title='Registrarse' error={errorMsg} disabled={loading} />
                 }
               }
             </RegisterMutation>
