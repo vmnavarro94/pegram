ec6ce37e6bc3cfe73e330cd280983a1cfa51f44f
vnavarro <vnavarro@brightcove.com>
2022-01-07 18:22:34 -0600

    Not found page added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
 import { Favs } from './pages/Favs'
 import { User } from './pages/User'
+import { NotFound } from './pages/NotFound'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { Context } from './Context'
 
@@ -18,12 +19,14 @@ export const App = () => {
       <GlobalStyle />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path='/' />
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
         {!isAuth && <NotRegisteredUser path='/login'/>}
         {!isAuth && <Redirect noThrow from='/favs' to='/login' />}
         {!isAuth && <Redirect noThrow from='/user' to='/login' />}
+        {isAuth && <Redirect from='/login' to='/' />}
         <Favs path='/favs' />
         <User path='/user' />
       </Router>
---
+++ src/pages/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const NotFound = () => (
+  <h1>Página no encontrada 404 :(</h1>
+)
